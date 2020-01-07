06a97bf610e232f15d1a00a7cf83697d73cf0243
mook <keettamook_k@cmu.ac.th>
2020-01-07 15:14:46 +0700

    finish lab 5_1

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -8,14 +8,29 @@ int main(){
 	
 	string name[N];
 	int age[N];
-	cout << "Name of student = ";
-	cout << "Age of student = ";
+	int i = 0;
+	while(i<N){
+		cout << "Name of student "<< i+1 <<" = ";
+		cin >> name[i];
+		cout << "Age of student "<< i+1 <<" = ";
+		cin >> age[i];
+		i++;
+
+	}
 	
 	int key;
 	cout << "--------------------------------------\n";
 	cout << "Enter an age to search: ";
 	cin >> key;
 	cout << "--------------------------------------\n";
+	i = 0;
+	while(i<N){
+		if(key==age[i]){
+			cout << name[i] << "\n";
+		}
+		i++;
+
+	}
 	
 
 	
